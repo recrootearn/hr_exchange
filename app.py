cf3bb36d9ceb7a2cab2e158fd620eac40265331e
recrootearn <info@recrootearn.com>
2026-06-19 13:12:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -403,6 +403,27 @@ def admin_only():
         return False
     return True
 
+@app.context_processor
+def inject_notifications():
+
+    unread_notifications = 0
+
+    try:
+
+        if current_user.is_authenticated:
+
+            unread_notifications = Notification.query.filter_by(
+                user_id=current_user.id,
+                is_read=False
+            ).count()
+
+    except:
+        pass
+
+    return dict(
+        unread_notifications=unread_notifications
+    )
+
 # =========================
 # USER ROUTES
 # =========================
@@ -584,27 +605,6 @@ def home():
 
         CandidateReview=CandidateReview,
         User=User
-    )
-
-@app.context_processor
-def inject_notifications():
-
-    unread_notifications = 0
-
-    try:
-
-        if current_user.is_authenticated:
-
-            unread_notifications = Notification.query.filter_by(
-                user_id=current_user.id,
-                is_read=False
-            ).count()
-
-    except:
-        pass
-
-    return dict(
-        unread_notifications=unread_notifications
     )
 
 @app.route('/my-uploads')
