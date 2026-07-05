71151379c42d7551dc783c83fd6834e6b1b2976b
recrootearn <info@recrootearn.com>
2026-07-05 18:20:43 +0530

    s

--- push_notification.py
+++ push_notification.py
@@ -1,24 +1,29 @@
-from firebase_admin import messaging
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+cred = credentials.Certificate("firebase-adminsdk.json")
+
+try:
+    firebase_admin.get_app()
+except ValueError:
+    firebase_admin.initialize_app(cred)
+
 
 def send_push_notification(token, title, body):
 
     message = messaging.Message(
-
         notification=messaging.Notification(
             title=title,
             body=body
         ),
-
         data={
             "title": title,
             "body": body
         },
-
         android=messaging.AndroidConfig(
             priority="high"
         ),
-
-        token=token
+        token=token,
     )
 
     return messaging.send(message)
