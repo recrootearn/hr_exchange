f3628fb2efb54226b61fcc3af828f9e4381ee1ed
recrootearn <info@recrootearn.com>
2026-07-07 18:19:31 +0530

    s

--- scheduler.py
+++ scheduler.py
@@ -1,28 +1,71 @@
-import time
+from datetime import datetime
 
-from app import app
-from app import db
-
-from app import BroadcastNotification
+from app import (
+    app,
+    db,
+    BroadcastNotification,
+    User,
+    CandidateUser,
+    send_notification
+)
 
 def check_notifications():
 
     with app.app_context():
 
-        print("Scheduler Running...")
+        print("Checking notifications...")
 
-if __name__ == "__main__":
+        notifications = BroadcastNotification.query.filter(
+            BroadcastNotification.status == "Scheduled",
+            BroadcastNotification.schedule_time <= datetime.now()
+        ).all()
 
-    print("Notification Scheduler Started")
+        for n in notifications:
 
-    while True:
+            full_message = f"{n.title}\n\n{n.message}"
 
-        try:
+            if n.send_to == "hr":
 
-            check_notifications()
+                for user in User.query.all():
 
-        except Exception as e:
+                    send_notification(
+                        user_id=user.id,
+                        user_type="hr",
+                        message=full_message,
+                        type="admin_broadcast"
+                    )
 
-            print("Scheduler Error:", e)
+            elif n.send_to == "candidate":
 
-        time.sleep(60)
+                for user in CandidateUser.query.all():
+
+                    send_notification(
+                        user_id=user.id,
+                        user_type="candidate",
+                        message=full_message,
+                        type="admin_broadcast"
+                    )
+
+            elif n.send_to == "both":
+
+                for user in User.query.all():
+
+                    send_notification(
+                        user_id=user.id,
+                        user_type="hr",
+                        message=full_message,
+                        type="admin_broadcast"
+                    )
+
+                for user in CandidateUser.query.all():
+
+                    send_notification(
+                        user_id=user.id,
+                        user_type="candidate",
+                        message=full_message,
+                        type="admin_broadcast"
+                    )
+
+            n.status = "Sent"
+
+        db.session.commit()
