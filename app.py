80b64b51b3d19bab4c4af616d54e17705a855706
recrootearn <info@recrootearn.com>
2026-07-13 13:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     mobile = db.Column(db.String(20), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120))
     company = db.Column(db.String(200))
     hr_type = db.Column(db.String(100))
     username = db.Column(db.String(100), unique=True)
